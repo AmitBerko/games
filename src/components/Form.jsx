24ff534e97e001c560f479471fc60f988e89ae6a
amitberko <amit.berko10@gmail.com>
2024-04-03 19:27:24 +0300

    Minor changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,66 +1,11 @@
 import React from 'react'
 
 function SubForm({ isLogin }) {
-	const handleToggle = () => {}
-	// const handleToggle = () => {
-	// 	const loginForm = document.getElementsByClassName('form-container')[0]
-	// 	const loginFormHeader = document.querySelector('.form-container.login-form .form-header')
-	// 	const registerFormHeader = document.querySelector('.form-container.register-form .form-header')
-	// 	const animationSettings = {
-	// 		duration: 1000,
-	// 		fill: 'forwards',
-	// 		easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)',
-	// 	}
-	// 	if (loginForm.classList.contains('shown')) {
-	// 		loginForm.classList.remove('shown')
-	// 		registerFormHeader.animate(
-	// 			{
-	// 				fontSize: '2.25rem',
-	// 			},
-	// 			animationSettings
-	// 		)
-	// 		loginFormHeader.animate(
-	// 			{
-	// 				fontSize: '1.5rem',
-	// 			},
-	// 			animationSettings
-	// 		)
-	// 		loginForm.animate(
-	// 			{
-	// 				transform: 'translate(-50%, 200px)',
-	// 				opacity: 1,
-	// 			},
-	// 			animationSettings
-	// 		)
-	// 	} else {
-	// 		loginForm.classList.add('shown')
-	// 		registerFormHeader.animate(
-	// 			{
-	// 				fontSize: '1.5rem',
-	// 			},
-	// 			animationSettings
-	// 		)
-	// 		loginFormHeader.animate(
-	// 			{
-	// 				fontSize: '2.25rem',
-	// 			},
-	// 			animationSettings
-	// 		)
-	// 		loginForm.animate(
-	// 			{
-	// 				transform: 'translate(-50%, -30%)',
-	// 				opacity: 1,
-	// 			},
-	// 			animationSettings
-	// 		)
-	// 	}
-	// }
-
 	return (
 		<div className={`form-container ${isLogin ? 'login-form' : 'register-form'}`}>
 			<label for="check">
 				<div className="form-header">
-					{isLogin ? 'Login' : 'Sign Up'}
+					{isLogin ? 'Login' : 'Register'}
 				</div>
 			</label>
 			<div className="form-padding">
@@ -82,7 +27,7 @@ function SubForm({ isLogin }) {
 							<i className="fa-solid fa-lock"></i>
 						</div>
 					)}
-					<button>{isLogin ? 'Login' : 'Sign Up'}</button>
+					<button>{isLogin ? 'Login' : 'Register'}</button>
 					<p className="login-register">
 						{isLogin ? `Don't have an account?` : 'Already have an account?'}{' '}
 						<label for="check">
