08ed79b38c918e3921d9d5d52cf9cedc4ed6989b
amitberko <amit.berko10@gmail.com>
2024-04-08 21:14:37 +0300

    Continued with the demo

--- src/pages/SpeedGame.jsx
+++ src/pages/SpeedGame.jsx
@@ -8,9 +8,16 @@ function SpeedGame() {
 	}
 
 	const [activeIndex, setActiveIndex] = useState(getRandomIndex())
+  const gameTime = 5000
+	const [timer, setTimer] = useState(gameTime) // start with 20 seconds
+	const [hasStarted, setHasStarted] = useState(false)
+	const [score, setScore] = useState(0)
 
 	const handleActiveClick = (e) => {
-    if (!e.isTrusted) return
+		// If it wasn't clicked by a human or game hasn't started, return
+		if (!e.isTrusted || !hasStarted) return
+
+		setScore((prevScore) => prevScore + 1)
 		setActiveIndex((prevIndex) => {
 			let newIndex = getRandomIndex()
 			while (newIndex === prevIndex) {
@@ -40,16 +47,16 @@ function SpeedGame() {
 		const handleContainerClick = (e) => {
 			const waveEffect = document.createElement('div')
 			waveEffect.classList.add('wave')
-      const { left, top } = tilesContainer.getBoundingClientRect()
+			const { left, top } = tilesContainer.getBoundingClientRect()
 			const x = e.clientX
 			const y = e.clientY
-			waveEffect.style.setProperty('--wave-x', (x - left) + 'px')
-			waveEffect.style.setProperty('--wave-y', (y - top) + 'px')
-      tilesContainer.appendChild(waveEffect)
+			waveEffect.style.setProperty('--wave-x', x - left + 'px')
+			waveEffect.style.setProperty('--wave-y', y - top + 'px')
+			tilesContainer.appendChild(waveEffect)
 
-      waveEffect.addEventListener('animationend', () => {
-        waveEffect.remove()
-      })
+			waveEffect.addEventListener('animationend', () => {
+				waveEffect.remove()
+			})
 		}
 
 		tilesContainer.addEventListener('mouseup', handleContainerClick)
@@ -59,20 +66,54 @@ function SpeedGame() {
 		}
 	}, [])
 
+	useEffect(() => {
+		let interval
+
+		if (hasStarted) {
+			interval = setInterval(() => {
+				setTimer((prevTimer) => {
+					if (prevTimer > 0) {
+						return prevTimer - 10
+					} else {
+            // GAME FINISHED
+            alert(`You scored ${score}`)
+						setHasStarted(false)
+						return 0
+					}
+				})
+			}, 10)
+		}
+
+    if (!hasStarted) {
+      setScore(0)
+      setTimer(gameTime)
+    }
+
+		// Cleanup function to clear the interval when the component unmounts or hasStarted changes
+		return () => clearInterval(interval)
+	}, [hasStarted, score])
+
 	return (
-		<div className="speed-game-wrapper">
-      <h1 className="speed-game-timer">30.00</h1>
-			<div className="tiles-container">
-				{Array.from({ length: tileCount }).map((_, index) => (
-					<Tile
-						handleActiveClick={handleActiveClick}
-						key={`tile${index}`}
-						id={`tile${index}`}
-						isActive={index === activeIndex}
-					/>
-				))}
+		<>
+			<div className="speed-game-wrapper">
+				<div className="speed-game-timer-score display-1 fw-medium">
+					<div className="speed-game-timer">{timer}</div>
+					<div className="speed-game-score">{score}</div>
+				</div>
+				<div className="tiles-container">
+					{Array.from({ length: tileCount }).map((_, index) => (
+						<Tile
+							handleActiveClick={handleActiveClick}
+							key={`tile${index}`}
+							id={`tile${index}`}
+							isActive={index === activeIndex}
+						/>
+					))}
+				</div>
 			</div>
-		</div>
+			<button onClick={() => setHasStarted(true)}>start</button>
+			<button onClick={() => setHasStarted(false)}>stop</button>
+		</>
 	)
 }
 
