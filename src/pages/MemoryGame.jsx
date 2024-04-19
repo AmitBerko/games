dba019dafd7957127e4bbf04f7d4af3f3779d6e5
amitberko <amit.berko10@gmail.com>
2024-04-19 18:05:13 +0300

    Made it responsive and fixed bugs

--- src/pages/MemoryGame.jsx
+++ src/pages/MemoryGame.jsx
@@ -9,7 +9,30 @@ function MemoryGame() {
 	const [disableTiles, setDisableTiles] = useState(true)
 	const [correctIndexesCount, setCorrectIndexesCount] = useState(0) // To prevent dom manipulation
 
-	function getRandomIndexes(gridLength) {
+	const levelConfig = {
+		1: { gridLength: 3, correctRatio: 0.4 },
+		2: { gridLength: 3, correctRatio: 0.45 },
+		3: { gridLength: 4, correctRatio: 0.4 },
+		4: { gridLength: 4, correctRatio: 0.45 },
+		5: { gridLength: 4, correctRatio: 0.5 },
+		6: { gridLength: 5, correctRatio: 0.35 },
+		7: { gridLength: 5, correctRatio: 0.4 },
+		8: { gridLength: 5, correctRatio: 0.45 },
+		9: { gridLength: 5, correctRatio: 0.5 },
+		10: { gridLength: 5, correctRatio: 0.55 },
+		11: { gridLength: 6, correctRatio: 0.35 },
+		12: { gridLength: 6, correctRatio: 0.4 },
+		13: { gridLength: 6, correctRatio: 0.45 },
+		14: { gridLength: 6, correctRatio: 0.5 },
+		15: { gridLength: 6, correctRatio: 0.55 },
+		16: { gridLength: 7, correctRatio: 0.35 },
+		17: { gridLength: 7, correctRatio: 0.4 },
+		18: { gridLength: 7, correctRatio: 0.45 },
+		19: { gridLength: 7, correctRatio: 0.5 },
+		20: { gridLength: 7, correctRatio: 0.55 },
+	}
+
+	function getRandomIndexes(gridLength, correctTilesRatio) {
 		// Create an array of all possible indexes
 		const indexes = Array.from({ length: gridLength * gridLength }, (_, index) => index)
 
@@ -19,43 +42,55 @@ function MemoryGame() {
 			;[indexes[i], indexes[j]] = [indexes[j], indexes[i]]
 		}
 
-		return indexes.slice(0, gridLength * gridLength * 0.45)
+		return indexes.slice(0, gridLength * gridLength * correctTilesRatio)
 	}
 
 	useEffect(() => {
 		document.documentElement.style.setProperty('--memory-grid-length', gridLength)
+		let gridGap = 0.6
+		if (gridLength === 5) {
+			gridGap = 0.5
+		} else if (gridLength === 6) {
+			gridGap = 0.45
+		} else if (gridLength === 7) {
+			gridGap = 0.4
+		}
+		document.documentElement.style.setProperty('--memory-grid-gap', `calc(${gridGap}rem + 0.275vw)`)
 	}, [gridLength])
 
 	useEffect(() => {
-		let newGridLength
-		if (level >= 3 && level <= 5) {
-			newGridLength = 4
-		} else if (level >= 6 && level <= 8) {
-			newGridLength = 5
-		} else {
-			newGridLength = 3 // Default grid length
-		}
-
-		// Update gridLength state
-		setGridLength(newGridLength)
-
-		// Update correctIndexes with new grid length
-		setCorrectIndexes(getRandomIndexes(newGridLength))
+		setGridLength(() => {
+			const { gridLength, correctRatio } =
+      // Use the current level's config. If it doesnt exist in the config, use the last one
+				levelConfig[level] || levelConfig[Object.keys(levelConfig).length]
+			setCorrectIndexes(getRandomIndexes(gridLength, correctRatio))
+			return gridLength
+		})
 	}, [level])
 
 	useEffect(() => {
+		if (triesLeft === 3) return // Means we just started, so don't do anything
+		const heartIcon = document.querySelector('.fa-heart')
+
+		// Make the heart shake
+		heartIcon.classList.add('fa-shake')
+		const handleAnimationEnd = () => {
+			// Stop the animation after it ends so it only runs once
+			heartIcon.classList.remove('fa-shake')
+		}
+
+		heartIcon.addEventListener('animationend', handleAnimationEnd)
 		if (triesLeft === 0) {
 			alert('you lost')
+			setDisableTiles(true)
+
+			// open up a results modal or something
+		}
+
+		return () => {
+			heartIcon.removeEventListener('animationend', handleAnimationEnd)
 		}
 	}, [triesLeft])
-
-	// useEffect(() => {
-	// 	// If you selected all correct tiles
-	// 	if (correctIndexesLength === currentCorrectIndexes.length && currentCorrectIndexes.length > 0) {
-	// 		setLevel((prevLevel) => prevLevel + 1)
-	//     setCorrectIndexesLength(0)
-	// 	}
-	// }, [correctIndexesLength])
 
 	useEffect(() => {
 		const correctTiles = [...document.querySelectorAll('.correct-tile')]
@@ -72,7 +107,7 @@ function MemoryGame() {
 	function handleTileClick(tileIndex) {
 		const tile = document.getElementsByClassName('memory-tile')[tileIndex]
 		if (!tile || disableTiles) return
-
+		if (tile.classList.contains('correct-tile') || tile.classList.contains('wrong-tile')) return
 		if (correctIndexes.includes(tileIndex)) {
 			// If correct
 			tile.classList.add('correct-tile')
@@ -102,31 +137,26 @@ function MemoryGame() {
 	}, [correctIndexesCount])
 
 	return (
-		<>
-			indexes are {correctIndexes}
-			<br></br>
-			disabled is {disableTiles.toString()}
-			{/* amount is {correctIndexesLength} */}
-			<div className="memory-game-container">
-				<div className="level-tries fs-1">
-					<p className="level">Level {level}</p>
-					<p className="tries">
-						{triesLeft} <i className="fa-solid fa-heart"></i>
-					</p>
-				</div>
-				<div className="memory-grid-container">
-					{Array.from({ length: gridLength * gridLength }).map((_, index) => (
-						<MemoryTile
-							key={`tile${index}`}
-							handleTileClick={() => handleTileClick(index)}
-							isCorrect={correctIndexes.includes(index)}
-							disableTiles={disableTiles}
-						/>
-					))}
-				</div>
-				<div></div>
+		<div className="memory-game-container">
+			<div className="level-tries fs-1">
+				<p className="level">Level {level}</p>
+				<p className="tries">
+					{triesLeft}{' '}
+					<i className="fa-solid fa-heart" style={{ '--fa-animation-iteration-count': 1 }}></i>
+				</p>
 			</div>
-		</>
+			<div className="memory-grid-container">
+				{Array.from({ length: gridLength * gridLength }).map((_, index) => (
+					<MemoryTile
+						key={`tile${index}`}
+						handleTileClick={() => handleTileClick(index)}
+						isCorrect={correctIndexes.includes(index)}
+						disableTiles={disableTiles}
+					/>
+				))}
+			</div>
+			<div></div>
+		</div>
 	)
 }
 
