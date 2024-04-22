e852587a8f422414465ed36155f82db8dee4de68
amitberko <amit.berko10@gmail.com>
2024-04-22 15:43:31 +0300

    Small changes

--- src/components/MemoryGameResults.jsx
+++ src/components/MemoryGameResults.jsx
@@ -7,17 +7,20 @@ function MemoryGameResults({showResults, handlePlayAgain, handleReturn}) {
 
 	return (
 		<>
-			<Modal show={showResults} onHide={handleReturn} backdrop="static">
-				<Modal.Header closeButton>
-					<Modal.Title>Game Results</Modal.Title>
+			<Modal show={showResults} onHide={handleReturn} backdrop="static" centered>
+				<Modal.Header>
+					<Modal.Title className="text-center w-100 fw-medium display-6">Game Results</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+				<Modal.Body className="text-center">
+					<div className="fs-4">You have reached level x</div>
+					<div className="fs-5">Your current best is: level y</div>
+				</Modal.Body>
 				<Modal.Footer>
-					<Button variant="secondary" onClick={handleReturn}>
-						Return To Homepage
-					</Button>
-					<Button variant="primary" onClick={handlePlayAgain}>
+					<Button variant="primary" onClick={handlePlayAgain} className="col-12 mx-0 col-sm me-sm-1">
 						Play Again
+					</Button>
+					<Button variant="secondary" onClick={handleReturn} className="col-12 mx-0 col-sm ms-sm-2">
+						Return To Homepage
 					</Button>
 				</Modal.Footer>
 			</Modal>
