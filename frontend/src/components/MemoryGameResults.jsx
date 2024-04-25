2947a2c5298a1a28cc0c74b651c4c3f03b0304de
amitberko <amit.berko10@gmail.com>
2024-04-25 22:20:08 +0300

    Minor changes

--- frontend/src/components/MemoryGameResults.jsx
+++ frontend/src/components/MemoryGameResults.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import { useAuth } from './AuthProvider'
 
-function MemoryGameResults({showResults, handlePlayAgain, handleReturn}) {
-
-
+function MemoryGameResults({ showResults, handlePlayAgain, handleReturn, level }) {
+	const { userData } = useAuth()
 	return (
 		<>
 			<Modal show={showResults} onHide={handleReturn} backdrop="static" centered>
@@ -12,11 +12,15 @@ function MemoryGameResults({showResults, handlePlayAgain, handleReturn}) {
 					<Modal.Title className="text-center w-100 fw-medium display-6">Game Results</Modal.Title>
 				</Modal.Header>
 				<Modal.Body className="text-center">
-					<div className="fs-4">You have reached level x</div>
-					<div className="fs-5">Your current best is: level y</div>
+					<div className="fs-4">You have reached level {level}</div>
+					<div className="fs-5">Your current best is: level {JSON.stringify(userData)} </div>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button variant="primary" onClick={handlePlayAgain} className="col-12 mx-0 col-sm me-sm-1">
+					<Button
+						variant="primary"
+						onClick={handlePlayAgain}
+						className="col-12 mx-0 col-sm me-sm-1"
+					>
 						Play Again
 					</Button>
 					<Button variant="secondary" onClick={handleReturn} className="col-12 mx-0 col-sm ms-sm-2">
