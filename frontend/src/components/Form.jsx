8a5d113aa6735102ac14a397d609571062da0992
amitberko <amit.berko10@gmail.com>
2024-04-26 22:40:12 +0300

    A signed in user now can't go back to `/login`

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import LoginForm from './LoginForm'
 import SignupForm from './SignupForm'
+import { useAuth } from './AuthProvider'
+import { Navigate } from 'react-router-dom'
 
 function Form() {
-  const handleCheckChange = () => {
-    const wrapper = document.querySelector('.wrapper')
-    const checkbox = document.querySelector('#check')
-    if (checkbox.checked) {
-      wrapper.classList.add('expand')
-    } else {
-      wrapper.classList.remove('expand')
-    }
-  }
+	const { userData } = useAuth()
+
+	const handleCheckChange = () => {
+		const wrapper = document.querySelector('.wrapper')
+		const checkbox = document.querySelector('#check')
+		if (checkbox.checked) {
+			wrapper.classList.add('expand')
+		} else {
+			wrapper.classList.remove('expand')
+		}
+	}
 
 	return (
-		<div className="wrapper">
-			<input id="check" type="checkbox" onChange={handleCheckChange} />
-      <LoginForm />
-      <SignupForm />
-		</div>
+		<>
+			{userData ? (
+				<Navigate to="/" />
+			) : (
+				<div className="wrapper">
+					<input id="check" type="checkbox" onChange={handleCheckChange} />
+					<LoginForm />
+					<SignupForm />
+				</div>
+			)}
+		</>
 	)
 }
 
