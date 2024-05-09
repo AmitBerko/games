46f232fa47123004ac666e4e7c48984ebfce2695
amitberko <amit.berko10@gmail.com>
2024-05-09 22:14:20 +0300

    Changed ui

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Table, Container, Form, Col } from 'react-bootstrap'
+import { Table, Container, Form, Col, Spinner } from 'react-bootstrap'
 import axios from '../api/axios'
 import BackButton from './BackButton'
 import LeaderboardRow from './LeaderboardRow'
@@ -7,6 +7,7 @@ import LeaderboardRow from './LeaderboardRow'
 function Leaderboard() {
 	const [leaderboardMode, setLeaderboardMode] = useState('Memory Game Best')
 	const [leaderboardData, setleaderboardData] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
 
 	const formatMode = () => {
 		return leaderboardMode.toLowerCase().split(' ')[0] + 'GameBest'
@@ -19,72 +20,41 @@ function Leaderboard() {
 			const response = await axios.post('/users/getLeaderboard', { leaderboardMode: formattedMode })
 			console.log(response.data)
 			setleaderboardData(response.data)
+			setIsLoading(false)
 		}
 
 		getLeaderboard()
 	}, [leaderboardMode])
 
 	return (
-		// <>
-		// <BackButton />
-		// 	<Container className="py-5">
-		// <Form.Select className="mb-5 w-auto" onChange={(e) => setLeaderboardMode(e.target.value)}>
-		// 	<option>Memory Game</option>
-		// 	<option>Speed Game</option>
-		// </Form.Select>
-		// 		<Col className="col col-sm-10 col-md-8 mx-auto">
-		// 			<Table striped bordered hover className="text-center leaderboard w-100">
-		// 				<thead className="p-5">
-		// 					<tr>
-		// 						<th>#</th>
-		// 						<th>Name</th>
-		// 						<th>Highest Level</th>
-		// 					</tr>
-		// 				</thead>
-		// 				<tbody>
-		// {leaderboardData.map((userData, index) => {
-		// 	return (
-		// 		<tr key={index + 1}>
-		// 			<td>{index}</td>
-		// 			<td>{userData.username}</td>
-		// 			<td>{userData[formatMode()]}</td>
-		// 		</tr>
-		// 	)
-		// })}
-		// 				</tbody>
-		// 			</Table>
-		// 		</Col>
-		// 		{/* </div> */}
-		// 	</Container>
-		// </>
-		<Container className="p-0">
-			{/* <div className="mode-back-container">
-        <BackButton />
-				<Form.Select className="mb-5" onChange={(e) => setLeaderboardMode(e.target.value)}>
-					<option>Memory Game</option>
-					<option>Speed Game</option>
-				</Form.Select>
-			</div> */}
-			{/* <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}> */}
-			<div className="leaderboard-container">
-				<LeaderboardRow
-					index="#"
-					name="Name"
-					level="select"
-					setLeaderboardMode={setLeaderboardMode}
-				/>
-				{leaderboardData.map((userData, index) => {
-					return (
+		<>
+			{isLoading ? (
+				<div className="spinner-container">
+					<Spinner className="leaderboard-spinner" />
+				</div>
+			) : (
+				<Container fluid className="p-0">
+					<div className="leaderboard-container">
 						<LeaderboardRow
-              key={index}
-							index={index + 1}
-							name={userData.username}
-							level={userData[formatMode()]}
+							index="#"
+							name="Name"
+							level="select"
+							setLeaderboardMode={setLeaderboardMode}
 						/>
-					)
-				})}
-			</div>
-		</Container>
+						{leaderboardData.map((userData, index) => {
+							return (
+								<LeaderboardRow
+									key={index}
+									index={index + 1}
+									name={userData.username}
+									level={userData[formatMode()]}
+								/>
+							)
+						})}
+					</div>
+				</Container>
+			)}
+		</>
 	)
 }
 
